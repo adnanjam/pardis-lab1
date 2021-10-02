6c8643661e4356ae1ac8c525885d165de35913d1
hovig <hovig@mail.com>
2021-10-02 11:45:11 +0200

    Add project structure

---
+++ src/test/java/SkipListTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SkipListTest {
+
+    @Test
+    void dummy() {
+        SkipList s = new SkipList();
+        assertEquals(s, s);
+    }
+}
