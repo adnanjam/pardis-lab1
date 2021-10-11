daeef4d61cbdae6b1265b241fe409b1b68416b66
pgegh <hovig@mail.com>
2021-10-11 20:12:39 +0200

    Changes by adnanjamil 2.0 final

--- src/main/java/Exercise3.java
+++ src/main/java/Exercise3.java
@@ -5,55 +5,70 @@ import java.util.Random;
 import java.util.concurrent.*;
 
 public class Exercise3 {
-    final static int MAX_OP = 1_000_000;
+    final static int MAX_OP = 1_000_0;
     final static int TOTAL_RUNS = 10;
-    final static int[] THREAD_COUNTS = new int[]{2,12,30,46};
+    final static int[] THREAD_COUNTS = new int[]{2, 12, 30, 46};
     final static double addProb = 0.1;
     final static double removeProb = 0.2;
     static Random r = new Random();
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        LockfreeConcurrentSkipListSet<Integer> list = new LockfreeConcurrentSkipListSet<>();
-        populateList(list, false); // Fills the list with elements
 
-        for(int threadCount : THREAD_COUNTS) {
-            // Average over ten runs
-            List<Long> results = new ArrayList<>(TOTAL_RUNS);
-            for(int run = 0; run < TOTAL_RUNS; run ++){
-                long start = System.nanoTime();
-                ExecutorService service = Executors.newFixedThreadPool(threadCount);
+        for (int normal = 0; normal < 2; normal++) {
 
-                for (int i = 0; i < MAX_OP; i++) {
-                    double p = r.nextDouble();
 
-                    if (p > removeProb) {
-                        // contains
-                        service.submit(() -> list.contains(r.nextInt()));
-                    } else if (p > addProb) {
-                        // remove
-                        service.submit(() -> list.remove(r.nextInt()));
-                    } else {
-                        // add
-                        service.submit(() -> list.add(r.nextInt()));
+            for (int threadCount : THREAD_COUNTS) {
+                LockfreeConcurrentSkipListSet<Integer> list = new LockfreeConcurrentSkipListSet<>();
+                populateList(list,  normal ); // Fills the list with elements
+
+                // Average over ten runs
+                List<Long> results = new ArrayList<>(TOTAL_RUNS);
+                for (int run = 0; run < TOTAL_RUNS; run++) {
+                    long start = System.nanoTime();
+                    ExecutorService service = Executors.newFixedThreadPool(threadCount);
+
+                    for (int i = 0; i < MAX_OP; i++) {
+                        double p = r.nextDouble();
+
+                        if (p > removeProb) { // larger than removeProb
+                            // contains
+                            service.execute(() -> list.contains(r.nextInt()));
+                        } else if (p > addProb) { // Smaller than removeProb and larger than addProb
+                            // remove
+                            service.execute(() -> list.remove(r.nextInt()));
+                        } else { // smaller than addProb and larger than 0
+                            // add
+                            service.execute(() -> list.add(r.nextInt()));
+                        }
                     }
+
+                    service.shutdown();
+                    while (true) {
+                        try {
+                            if (service.awaitTermination(500, TimeUnit.MICROSECONDS)) {
+                                break;
+                            }
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    results.add(System.nanoTime() - start);
                 }
-                service.shutdown();
-                results.add(System.nanoTime() - start);
+
+                long avgExecutionTime = arraySum(results) / TOTAL_RUNS;
+
+                System.out.println(threadCount + ",  " + avgExecutionTime + ", " + normal + ", " +  + avgExecutionTime / 1_000_000);
             }
-
-            long avgExecutionTime = arraySum(results) / TOTAL_RUNS;
-
-            System.out.println( threadCount + ",  " + avgExecutionTime + ", " + avgExecutionTime / 1_000_000);
-
         }
     }
 
-    private static void populateList(LockfreeConcurrentSkipListSet list, boolean normal) {
+    private static void populateList(LockfreeConcurrentSkipListSet list, int normal) {
         List<Integer> population;
 
-        if(normal){
+        if (normal == 1) {
             population = Populator.normal();
-        }else{
+        } else {
             population = Populator.uniform();
         }
 
@@ -62,9 +77,9 @@ public class Exercise3 {
         }
     }
 
-    private static long arraySum (List<Long> array){
+    private static long arraySum(List<Long> array) {
         long sum = 0;
-        for (int i = 0 ; i < array.size();i++){
+        for (int i = 0; i < array.size(); i++) {
             sum += array.get(i);
         }
 
