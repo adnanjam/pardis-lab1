ba158d611f746428286163f81424b4bbeac63ce2
pgegh <hovig@mail.com>
2021-10-13 21:33:09 +0200

    Changes by adnanjamil

--- src/main/java/Populator.java
+++ src/main/java/Populator.java
@@ -1,17 +1,32 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.locks.Lock;
 
 public class Populator {
-    static final int POPULATION_SIZE = 10_000;
+    static final int POPULATION_SIZE = 100_000;
     static final int MAX_VALUE = 10_000_000;
     static final double variance = 0.1;
     static final double mean = 0.5;
 
-    public static void main(String[] args){
-        Random rand = new Random();
+    public static void main(String[] args) {
+        List<Integer> population = uniform();
+        double n = population.size();
 
-        System.out.println();
+        Double c_mean = 0.0;
+        for (int i = 0; i < n; i++) {
+            c_mean += (double) population.get(i) / n;
+        }
+
+        double c_variance = 0;
+        for (int i = 0; i < n; i++) {
+            c_variance += Math.pow(c_mean - (double) population.get(i), 2.0) / (double) n;
+        }
+
+        System.out.println("N: " + n + " Mean: " + c_mean + " Variance: " + Math.sqrt(c_variance));
+
     }
 
     public static List<Integer> normal() {
